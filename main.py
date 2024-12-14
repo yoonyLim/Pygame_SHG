96b9a40789f25a6ac2d1360029df1354c5de0f4e
Hayoon Lim / 임하윤 <64838255+yoonyLim@users.noreply.github.com>
2024-12-14 12:18:34 +0900

    update: UI update

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import content.simulation_manger as sim
 from content.physics.cluster import Cluster
 from content.physics.particle import Particle
 from content.UI.textbox import TextBox
+from content.UI.button import Button
 
 def main():
     pg.init()
@@ -14,12 +15,29 @@ def main():
     while sim.SIM_LOOP:
         sim.CLOCK.tick(sim.FPS)
 
+        FPS_text = TextBox(10, 10, "FPS: " + str(round(sim.CLOCK.get_fps(), 2)), sim.FONT_FAMILY, sim.FONT_SIZE, sim.FONT_COLOR, "topleft")
+        num_particles_text = TextBox(10, 30, "# of Particles: " + str(sim.NUM_PARTICLES), sim.FONT_FAMILY, sim.FONT_SIZE, sim.FONT_COLOR, "topleft")
+        using_SHG_text = TextBox(10, 50, "Using SHG: " + str(sim.USE_SHG), sim.FONT_FAMILY, sim.FONT_SIZE, sim.FONT_COLOR, "topleft")
+        chasing_mouse_text = TextBox(10, 70, "Chasing Mouse: " + str(sim.PLANET_LIKE), sim.FONT_FAMILY, sim.FONT_SIZE, sim.FONT_COLOR, "topleft")
+
+        change_mode_btn = Button(sim.SCREEN_WIDTH - 100, 20, "| Toggle SHG |", sim.FONT_FAMILY, sim.FONT_SIZE, sim.FONT_COLOR, 0)
+        chase_mouse_btn = Button(sim.SCREEN_WIDTH - 100, 50, "| Toggle Chase Mouse |", sim.FONT_FAMILY, sim.FONT_SIZE, sim.FONT_COLOR, 1)
+
+        TXT_LIST = [FPS_text, num_particles_text, using_SHG_text, chasing_mouse_text]
+        BTN_LIST = [change_mode_btn, chase_mouse_btn]
+
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 sim.SIM_LOOP = False
             elif event.type == pg.MOUSEBUTTONDOWN:
-                new_particle = Particle(pg.mouse.get_pos()[0] * 1.0, pg.mouse.get_pos()[1] * 1.0, mass=100, radius=30.0, color=(255, 0, 0, 255))
-                cluster.particles.append(new_particle)
+                if change_mode_btn.checkForInput(pg.mouse.get_pos()):
+                    sim.USE_SHG = not sim.USE_SHG
+                elif chase_mouse_btn.checkForInput(pg.mouse.get_pos()):
+                    sim.PLANET_LIKE = not sim.PLANET_LIKE
+                else:
+                    new_particle = Particle(pg.mouse.get_pos()[0] * 1.0, pg.mouse.get_pos()[1] * 1.0, mass=100, radius=30.0, color=(255, 0, 0, 255))
+                    cluster.particles.append(new_particle)
+                    sim.NUM_PARTICLES += 1
 
         if sim.PLANET_LIKE:
             sim.PLANET_CENTER = [pg.mouse.get_pos()[0], pg.mouse.get_pos()[1]]
@@ -34,16 +52,16 @@ def main():
         
         cluster.draw(sim.SCREEN)
 
-        FPS_text = TextBox(10, 10, "FPS: " + str(round(sim.CLOCK.get_fps(), 2)), sim.FONT_FAMILY, sim.FONT_SIZE, sim.FONT_COLOR, "topleft")
-        num_particles_text = TextBox(10, 30, "# of Particles: " + str(sim.NUM_PARTICLES), sim.FONT_FAMILY, sim.FONT_SIZE, sim.FONT_COLOR, "topleft")
-
-        TXT_LIST = [FPS_text, num_particles_text]
-
         for texts in TXT_LIST:
             texts.update()
 
+        for btn in BTN_LIST:
+            btn.update()
+
         ## update screen ##
         pg.display.update()
+        
+        print(sim.CLOCK.get_fps())
 
     pg.quit()
     sys.exit()
